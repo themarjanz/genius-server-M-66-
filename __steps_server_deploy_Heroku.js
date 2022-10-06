0ba050ebc6ae28e8e0989f9c422dcc450c7230f3
themarjanz <themarjanz@gmail.com>
2022-10-06 21:38:53 +0600

    adding steps

---
+++ __steps_server_deploy_Heroku.js
@@ -0,0 +1,14 @@
+/*
+-------------------------
+One time for your computer
+--------------------------
+1.create heroku account
+2.verify email
+3.instll heroku cli
+4.heroku login
+-----------------------------
+FOR EACH PROJECT  ONE TIME
+--------------------------------
+1.heroku create
+2.git push heroku main
+*/ 
